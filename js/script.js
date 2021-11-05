1b53b6b52740602baa825db5e034af6c178935a5
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-05 14:28:22 +0100

    basic input handling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const addItemBtn = $('.to-do__btn');
-
+const toDoInput = $('#add-item');
 const toDoList = $('.to-do__list');
 
-toDoList.append('<li>learn jquery</li>')
+toDoList.append('<li>learn jquery</li>');
 
+addItemBtn.on('click', addItemHandler);
+
+function addItemHandler() {
+    const newItem = toDoInput.val();
+    toDoList.append(`<li>${newItem}</li`);
+    toDoInput.val('');
+}
