ba18559080fa4d403f89090ff79e70186b4aafc5
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-08 10:17:04 +0100

    stored new elements in variables, addes all logic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const addItemBtn = $('#button');
 const toDoInput = $('#input');
 const toDoList = $('#list');
+toDoList.sortable();
 
-toDoList.append('<li>learn jquery</li>');
 
 addItemBtn.on('click', addItemHandler);
 
@@ -10,12 +10,24 @@ addItemBtn.on('click', addItemHandler);
 function addItemHandler(e) {
     e.preventDefault();
     const newItem = toDoInput.val();
-    const listItem = `<li>${newItem}</li>`;
+    if (!newItem) {
+        return alert('Please enter an item');
+    }
+    const listItem = $(`<li>${newItem}</li>`);
     toDoList.append(listItem);
-    $('li').last().on('click', strike);
+    // add event listener
+    listItem.on('dblclick', strike.bind(null,listItem));
+    // add delete button
+    const deleteBtn = $('<button class="crossOutButton">X</button>');
+    listItem.append(deleteBtn);
+    deleteBtn.on('click', deleteItem.bind(null, listItem))
     toDoInput.val('');
 }
 
-function strike() {
-    this.classList.toggle('strike')   
+function strike(item) {
+    item.toggleClass('strike');
+}
+
+function deleteItem(item) {
+    item.remove();
 }
